b657d78419d55a954a234a5722c568cc6d8270a4
Edy <edyxicon@gmail.com>
2023-01-02 13:34:18 -0600

    git init

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,21 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+})
+export class BusquedaComponent {
+  @ViewChild('textBuscar') textBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) {}
+
+  buscar() {
+    const valor = this.textBuscar.nativeElement.value;
+    if (valor.trim().length === 0) {
+      return;
+    }
+    this.gifsService.buscarGifs(valor);
+    this.textBuscar.nativeElement.value = '';
+  }
+}
